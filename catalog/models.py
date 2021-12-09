1d0d8ebf61ca224c35795b25e114689e6d1e82bb
Boris <yanushko64@gmail.com>
2021-12-09 21:57:01 +0300

    add Tank, tanknation, tanktype classes

--- catalog/models.py
+++ catalog/models.py
@@ -3,11 +3,13 @@ from django.urls import reverse
 
 
 class Gold(models.Model):
-
     title = models.CharField(max_length=200)
     description = models.TextField()
     price = models.IntegerField(default=0)
+    promo = models.BooleanField(default=False)
     discount = models.IntegerField(default=0)
+    display = models.BooleanField(default=True)
+    priority = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
@@ -25,11 +27,13 @@ class GoldImage(models.Model):
 
 
 class Premium(models.Model):
-
     title = models.CharField(max_length=200)
     description = models.TextField()
     price = models.IntegerField(default=0)
+    promo = models.BooleanField(default=False)
     discount = models.IntegerField(default=0)
+    display = models.BooleanField(default=True)
+    priority = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
@@ -44,3 +48,44 @@ class PremiumImage(models.Model):
         on_delete=models.CASCADE,
         related_name='images')
     image = models.ImageField(upload_to='premium')
+
+
+class TankType(models.Model):
+    name = models.CharField(max_length=200, help_text="Enter a tank type", null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class TankNation(models.Model):
+    name = models.CharField(max_length=200, help_text="Enter a tank nation", null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Tank(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.IntegerField(default=0)
+    promo = models.BooleanField(default=False)
+    discount = models.IntegerField(default=0)
+    display = models.BooleanField(default=True)
+    priority = models.BooleanField(default=False)
+    type = models.ManyToManyField(TankType, help_text="Select a type for this tank")
+    nation = models.ManyToManyField(TankNation, help_text="Select a nation for this tank")
+    tier = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('tank-detail', args=[str(self.id)])
+
+
+class TankImage(models.Model):
+    tank = models.ForeignKey(
+        Tank,
+        on_delete=models.CASCADE,
+        related_name='images')
+    image = models.ImageField(upload_to='tanks')
